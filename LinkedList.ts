d833adb235a9eef92e8db31cea9fcc90fea13663
Charbytesdev <138222011+Charbytesdev@users.noreply.github.com>
2023-12-16 15:23:55 +0200

    Implement Linked-List append function

--- LinkedList.ts
+++ LinkedList.ts
@@ -13,3 +13,25 @@ export default class LinkedList {
   set head(head: Node) {
     this._head = head;
   }
+
+  append(value: number) {
+    const insertedNode = new Node(value, null);
+    if (this.head == null) {
+      this.head = insertedNode;
+    } else {
+      let headCopy = this.head;
+      while (headCopy.next !== null) {
+        headCopy = headCopy.next;
+      }
+      headCopy.next = insertedNode;
+    }
+  }
+}
+
+// const ll = new LinkedList();
+// ll.append(2);
+// ll.append(3);
+// ll.append(5);
+// ll.append(5);
+
+// console.log((ll.head as Node).next);
